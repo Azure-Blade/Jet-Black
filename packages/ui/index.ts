8ccf6638bd26d9d6411309f264724668a133b2ad
Fractal-Tess <vgfractal@gmail.com>
2022-11-26 23:55:39 +0200

    spacing

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,2 +1,2 @@
 export { default as Button } from './components/button/Button.svelte'
-export { default as ThemeToggle} from './components/theme/ThemeToggle.svelte'
+export { default as ThemeToggle } from './components/theme/ThemeToggle.svelte'
