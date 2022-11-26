7344f2689b82b25cc8ae24e579d2a8cd186ef438
Fractal-Tess <vgfractal@gmail.com>
2022-11-26 23:56:01 +0200

    removed template routes

--- apps/tauri/src-tauri/src/main.rs
+++ apps/tauri/src-tauri/src/main.rs
@@ -1,4 +1,3 @@
-#![allow(unused)] //Remove later
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
@@ -8,32 +7,11 @@ mod commands;
 mod error;
 mod prelude;
 
-use crate::{
-    commands::{called_from_js, hash256sum},
-    prelude::*,
-};
+use tauri::{generate_context, Builder};
 
-use tauri::{generate_context, generate_handler, Builder, Manager, RunEvent};
-
-fn main() -> Result<()> {
-    let app = Builder::default()
-        .plugin(tauri_plugin_store::PluginBuilder::default().build())
+fn main() {
+    Builder::default()
         .plugin(tauri_plugin_window_state::Builder::default().build())
-        .invoke_handler(generate_handler![called_from_js, hash256sum])
-        .build(generate_context!())
+        .run(generate_context!())
         .expect("error while running tauri application");
-
-    app.run(|app_handle, e| match e {
-        RunEvent::Ready => {
-            let window = app_handle
-                .get_window("main")
-                .expect("Cannot get main window");
-            window.show();
-
-            pl!("Window is ready");
-        }
-        _ => {}
-    });
-
-    Ok(())
 }
