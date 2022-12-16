b1b1b68aa4a6eb4036adfc8c13c348124055eebf
Fractal-Tess <vgfractal@gmail.com>
2022-12-16 23:11:08 +0200

    move types

--- apps/scrapper/src/types.ts
+++ apps/scrapper/src/types.ts
@@ -1,18 +1,9 @@
+import { z } from 'zod'
+import { validator as mangaInfoValidator } from './validators/mangainfo.js'
+
 export type Resource = {
   title: string
   url: URL
 }
-export interface DBMeta {
-  collectionId: string
-  collectionName: string
-  updated: string
-  created: string
-  id: string
-  expand: {}
-}
 
-export type InsertableHTML = {
-  url: URL
-  html: string
-}
-export type HTMLCollection = InsertableHTML & DBMeta
+export type MangaInfo = z.infer<typeof mangaInfoValidator>
